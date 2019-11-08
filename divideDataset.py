71e1ac65ec972907df2da2543cf49fc16a32f33c
Abhishek Jajal <35066902+abhijajal@users.noreply.github.com>
2019-11-07 19:26:39 -0600

    Add files via upload

--- divideDataset.py
+++ divideDataset.py
@@ -1,4 +1,4 @@
-dataSetFile = open("tweetDataset.txt",'r')
+dataSetFile = open("oldTweets.txt",'r')
 lines = dataSetFile.readlines()
 i=1
 fileDivya = open("tweetDataset_Divya.txt",'w')
@@ -8,7 +8,7 @@ fileJacky = open("tweetDataset_Jacky.txt",'w')
 fileJajal = open("tweetDataset_Jajal.txt",'w')
 for eachLine in lines:
     if(i>0 and i<=1000):
-        fileDivya.write(eachLine)
+        fileJajal.write(eachLine)
     elif(i>1000 and i<=2000):
         fileMatt.write(eachLine)
     elif (i > 2000 and i <= 3000):
@@ -16,5 +16,5 @@ for eachLine in lines:
     elif (i > 3000 and i <= 4000):
         fileJacky.write(eachLine)
     elif (i > 4000):
-        fileJajal.write(eachLine)
+        fileDivya.write(eachLine)
     i=i+1
