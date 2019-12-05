4be9b835bdeea1ce43715de6bd26b0cda0f7f6d7
Abhishek Jajal <35066902+abhijajal@users.noreply.github.com>
2019-12-05 12:20:51 -0600

    Add files via upload

---
+++ spacy_test.py
@@ -0,0 +1,39 @@
+import re
+import string
+import nltk
+import spacy
+import pandas as pd
+import numpy as np
+import math
+from tqdm import tqdm
+
+from spacy.matcher import Matcher
+from spacy.tokens import Span
+from spacy import displacy
+
+pd.set_option('display.max_colwidth', 200)
+
+
+def num_patterns(txt):
+
+	nlp = spacy.load("en_core_web_sm")
+	doc = nlp(txt)
+	print(txt)
+	# Matcher class object
+	matcher = Matcher(nlp.vocab)
+	matcher.add("matching", None, [{'POS': 'NUM'}, {'POS': 'VERB'}], [{'POS': 'NUM'}, {'POS': 'NOUN'}])
+
+	matches = matcher(doc)
+	span=""
+	for match_id, start, end in matches:
+		span = doc[start:end]
+		print(span.text)
+
+
+newPositiveDataSetFile = open("positiveDataset.txt", 'r+')
+
+lines = newPositiveDataSetFile.readlines()
+for eachLine in lines:
+
+	num_patterns(eachLine)
+
---
+++ tweet_producer_keyword.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+
+from kafka import KafkaProducer
+from time import sleep
+import json, sys
+import requests
+import time
+import tweepy
+import sys
+import apiKeys
+
+def publish_message(producer_instance, topic_name, value):
+    try:
+        key_bytes = bytes('foo', encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
+        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.flush()
+        print('Message published successfully.')
+    except Exception as ex:
+        print('Exception in publishing message')
+        print(str(ex))
+
+def connect_kafka_producer():
+    _producer = None
+    try:
+        #_producer = KafkaProducer(value_serializer=lambda v:json.dumps(v).encode('utf-8'),bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
+         _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10),linger_ms=10)
+    
+    except Exception as ex:
+        print('Exception while connecting Kafka')
+        print(str(ex))
+    finally:
+        return _producer
+
+class CustomStreamListener(tweepy.StreamListener):
+    count = 1
+    def on_status(self, status):
+            if(status._json['lang']=='en'):#Checking if the tweet is in english
+                print(self.count)
+                try:
+                    tweet= status._json['extended_tweet']['full_text'].replace('\n',' ').strip()+"::::::"+status._json['created_at']
+                except:
+                    tweet=status.text.replace('\n',' ').strip()+"::::::"+status._json['created_at']
+
+                prod = connect_kafka_producer();
+
+                print(json.dumps(tweet))
+                publish_message(prod, 'twitter', tweet)
+
+                if prod is not None:
+                    prod.close()
+                self.count=self.count+1
+                file.write(tweet+"\n")
+                data.write(str(status._json) + "\n")
+                sleep(10)
+
+                if not status._json['retweeted'] and 'RT @' not in status._json['text']:
+                    #Checking if the tweet is in english
+                    print(self.count)
+                    try:
+                        tweet= status._json['extended_tweet']['full_text'].replace('\n',' ').strip()+"::::::"+status._json['created_at']
+                    except:
+                        tweet=status.text.replace('\n',' ').strip()+"::::::"+status._json['created_at']
+
+                    prod = connect_kafka_producer();
+
+                    print(json.dumps(tweet))
+                    publish_message(prod, 'twitter', tweet)
+
+                    if prod is not None:
+                        prod.close()
+                    self.count=self.count+1
+                    file.write(tweet+"\n")
+                    data.write(str(status._json) + "\n")
+                    sleep(10)
+
+
+    def on_error(self, status_code):
+        print >> sys.stderr, 'Encountered error with status code:', status_code
+        return True # Don't kill the stream
+
+    def on_timeout(self):
+        print >> sys.stderr, 'Timeout...'
+        return True # Don't kill the stream
+
+
+
+if __name__== "__main__":
+    
+    if len(sys.argv) != 1:
+        print ('Number of arguments is not correct')
+        exit()
+
+    consumer_key = apiKeys.consumer_key
+    consumer_secret = apiKeys.consumer_secret
+    access_key = apiKeys.access_key
+    access_secret = apiKeys.access_secret
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    data = open("data.json", "a+")
+    file = open("tweets.txt", "a+")
+
+    sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
+    # keywords on which we will filter
+    # Texas:-
+    #sapi.filter(locations=[-105.705273, 27.022092, -93.861037, 36.593243])
+
+    # DFW:-
+    #sapi.filter(locations=[-97.458277, 32.725528, -97.230735, 32.778289])
+    # Dallas:-
+    sapi.filter(track=['accident'], is_async=True)
